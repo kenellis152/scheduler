8c580041e7276340528511c690c1b959acb0a0d4
Ken Ellis <kenellis152@gmail.com>
2017-04-02 21:50:20 -0400

    added placeholder flot chart and a few style changes

--- public/components/resinDashboard/resinDash.component.js
+++ public/components/resinDashboard/resinDash.component.js
@@ -5,7 +5,7 @@ angular.module('scheduler')
 .component('resinDash', {
   templateUrl: 'components/resinDashboard/resinDash.component.html',
   bindings: {
-    plant: '<'
+    plantid: '<'
   },
   controller: resinDashController
 });
@@ -23,9 +23,95 @@ function resinDashController ($scope) {
   });
 
   $scope.$on('namespace:plantinfo', function (event, data) {
-    $ctrl.plant = data.plants[1];
+    $ctrl.plant = data.plants[$ctrl.plantid];
+    $ctrl.updateDash();
   });
 
+  $ctrl.updateDash = function () {
+
+  };
+
+  var data1 = [
+    [-8, 4],
+    [-7, 8],
+    [-6, 5],
+    [-5, 10],
+    [-4, 4],
+    [-3, 16],
+    [-2, 5],
+    [-1, 11],
+    [0, 6],
+    [1, 11],
+    [2, 30],
+    [3, 10],
+    [4, 13],
+    [5, 4],
+    [6, 3],
+    [7, 3],
+    [8, 6]
+];
+var data2 = [
+    [-8, 1],
+    [-7, 0],
+    [-6, 2],
+    [-5, 0],
+    [-4, 1],
+    [-3, 3],
+    [-2, 1],
+    [-1, 5],
+    [0, 2],
+    [1, 3],
+    [2, 2],
+    [3, 1],
+    [4, 0],
+    [5, 2],
+    [6, 8],
+    [7, 0],
+    [8, 0]
+];
+
+var options = {
+    series: {
+        lines: {
+            show: false,
+            fill: true
+        },
+        splines: {
+            show: true,
+            tension: 0.4,
+            lineWidth: 1,
+            fill: 0.4
+        },
+        points: {
+            radius: 0,
+            show: true
+        },
+        shadowSize: 2,
+        grow: {stepMode:"linear",stepDirection:"up",steps:80}
+    },
+    grow: {stepMode:"linear",stepDirection:"up",steps:80},
+    grid: {
+        hoverable: true,
+        clickable: true,
+        tickColor: "#d5d5d5",
+        borderWidth: 1,
+        color: '#d5d5d5'
+    },
+    colors: ["#1ab394", "#1C84C6"],
+    xaxis: {
+    },
+    yaxis: {
+        ticks: 4
+    },
+    tooltip: false
+};
+
+/**
+ * Definition of variables
+ * Flot chart
+ */
+$ctrl.flotData = [data1, data2];
+$ctrl.flotOptions = options;
 
 }
 
--- public/js/config.js
+++ public/js/config.js
@@ -46,6 +46,11 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
                         {
                             name: 'ui.sortable',
                             files: ['js/plugins/ui-sortable/sortable.js']
+                        },
+                        {
+                            serie: true,
+                            name: 'angular-flot',
+                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
                         }
                     ]);
                 }
