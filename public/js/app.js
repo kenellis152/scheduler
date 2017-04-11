3c48e1b3a91c1fd6a09fd98034823de462212912
Ken Ellis <kenellis152@gmail.com>
2017-04-10 22:39:48 -0400

    attempt at fixing angular ApiPath to resolve based on environment

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,16 @@
  * INSPINIA - Responsive Admin Theme
  *
  */
+var host = window.location.hostname;
+var ApiPath;
+if (host === 'localhost') {
+    ApiPath = 'http://localhost:3000';
+} else {
+  ApiPath = 'http://glacial-everglades-27245.herokuapp.com';
+}
+
+
+
 (function () {
     angular.module('scheduler', [
         'ui.router',                    // Routing
@@ -9,5 +19,5 @@
         'ui.bootstrap',                 // Ui Bootstrap
         'ngMaterial'
     ])
-    .constant('ApiPath', 'http://localhost:3000')
+    .constant('ApiPath', ApiPath)
 })();
